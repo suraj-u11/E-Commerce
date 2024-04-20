f10685c986aba377a997c2e0110b53616c6f6ed6
suraj.u <757.suraj@gmail.com>
2024-04-21 01:12:31 +0530

    Used Spring Data JPA and connected to MySQL DB and created 2 tables using Model classes

--- src/main/java/dev/suraj/productservice/models/Product.java
+++ src/main/java/dev/suraj/productservice/models/Product.java
@@ -1,15 +1,19 @@
 package dev.suraj.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    private Long id;
+@Entity
+public class Product extends BaseModel{
     private String title;
     private double price;
-    private Category category;
-    private String description;
     private String imageUrl;
+    private String description;
+
+    @ManyToOne
+    private Category category;
 }
