8ae0c862ddf16df3ec892ae64415e79d805c8d60
suraj.u <757.suraj@gmail.com>
2024-04-28 16:43:17 +0530

    Exploring Different Fetch Modes and Types, also test functions to test the same

--- src/main/java/dev/suraj/productservice/models/Category.java
+++ src/main/java/dev/suraj/productservice/models/Category.java
@@ -3,9 +3,12 @@ package dev.suraj.productservice.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -14,7 +17,8 @@ import java.util.List;
 @Entity
 public class Category extends BaseModel{
     private String categoryName;
-    @OneToMany(mappedBy = "category", cascade = {CascadeType.REMOVE})
+    @OneToMany(mappedBy = "category", cascade = {CascadeType.REMOVE}, fetch = FetchType.LAZY)
     @JsonIgnore
+    @Fetch(FetchMode.SELECT)
     private List<Product> products;
 }
--- src/test/java/dev/suraj/productservice/ProductServiceApplicationTests.java
+++ src/test/java/dev/suraj/productservice/ProductServiceApplicationTests.java
@@ -4,6 +4,7 @@ import dev.suraj.productservice.models.Category;
 import dev.suraj.productservice.models.Product;
 import dev.suraj.productservice.repositories.CategoryRepo;
 import dev.suraj.productservice.repositories.ProductRepo;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,7 +31,7 @@ class ProductServiceApplicationTests {
 
     @Test
     public void testFindCategoryByTitle() {
-        Category category = categoryRepo.findByCategoryName("electronics");
+        Category category = categoryRepo.findByCategoryName("clothing");
         System.out.println(category.getCategoryName());
     }
 
@@ -38,6 +39,15 @@ class ProductServiceApplicationTests {
     public void testFindProductById() {
         Product product = productRepo.findProductByIdIsAndIsDeletedFalse(52L);
         System.out.println(product.getId() + " " + product.getCategory().getCategoryName());
+    }
+
+    @Test
+    @Transactional
+    public void testCategoryBasedProducts() {
+        Category category = categoryRepo.findByCategoryName("clothing");
+        System.out.println(category.getCategoryName());
+        List<Product> products = category.getProducts();
+        System.out.println(products);
     }
 
 }
